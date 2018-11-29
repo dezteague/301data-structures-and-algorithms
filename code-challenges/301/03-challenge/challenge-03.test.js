6e6de672b77ff89dce7d98dcd8ce478b84fc8486
Deziree Teague <dez.teague@gmail.com>
2018-11-29 09:13:28 -0800

    working on 4

--- code-challenges/301/03-challenge/challenge-03.test.js
+++ code-challenges/301/03-challenge/challenge-03.test.js
@@ -60,7 +60,10 @@ For example, ['Alphabet', 'alphabet', 'carrot', 'Zebra'] is correctly sorted, an
 ------------------------------------------------------------------------------------------------ */
 
 const alphabetizeBetter = (arr) => {
-  // Solution code here...
+	return arr.sort((a,b) => {
+		
+	}
+	)
 };
 
 /* ------------------------------------------------------------------------------------------------
